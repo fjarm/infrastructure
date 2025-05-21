17da38984e6b4ba3df2610292f1dfa66132db1d0
Jeremy Muhia <jmuhia80@gmail.com>
2025-05-21 15:22:22 -0400

    Use pulumi context instead of Go library context

--- cert-manager/v1/pkg/release/cert_manager_helm_release.go
+++ cert-manager/v1/pkg/release/cert_manager_helm_release.go
@@ -1,9 +1,9 @@
 package release
 
 import (
-	"context"
 	"github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes"
 	helmv3 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/helm/v3"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 const (
@@ -13,6 +13,6 @@ const (
 	chartVersion   = "1.17.2"
 )
 
-func NewCertManagerHelmRelease(ctx context.Context, k8sProvider *kubernetes.Provider) (*helmv3.Release, error) {
+func NewCertManagerHelmRelease(ctx *pulumi.Context, k8sProvider *kubernetes.Provider) (*helmv3.Release, error) {
 	return nil, ErrUnimplemented
 }
