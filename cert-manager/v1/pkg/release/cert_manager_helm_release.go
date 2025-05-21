ead2782e582073d667bbae354ed75548acc24108
Jeremy Muhia <jmuhia80@gmail.com>
2025-05-21 14:53:07 -0400

    Set up unimplemented cert-manager helm release

--- cert-manager/v1/pkg/release/cert_manager_helm_release.go
+++ cert-manager/v1/pkg/release/cert_manager_helm_release.go
@@ -1,8 +1,18 @@
 package release
 
+import (
+	"context"
+	"github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes"
+	helmv3 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/helm/v3"
+)
+
 const (
 	chartName      = "cert-manager"
 	chartNamespace = "cert-manager"
 	chartRepo      = "https://charts.jetstack.io"
 	chartVersion   = "1.17.2"
 )
+
+func NewCertManagerHelmRelease(ctx context.Context, k8sProvider *kubernetes.Provider) (*helmv3.Release, error) {
+	return nil, ErrUnimplemented
+}
