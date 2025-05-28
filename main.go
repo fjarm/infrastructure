9c48790270c0a8468f462224523fa52fbb8b47f7
Jeremy Muhia <jmuhia80@gmail.com>
2025-05-27 20:13:47 -0400

    Track root level main.go

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"github.com/fjarm/infrastructure/pkg/v1/certmanager"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		return nil
+		err := certmanager.DeployCertManager(ctx)
+		return err
 	})
 }
