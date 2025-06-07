e1dbfee18ea2f1f079d460e7ba1becde7a8f7a95
Jeremy Muhia <jmuhia80@gmail.com>
2025-06-07 19:48:09 -0400

    Deploy single node dragonfly instance with server auth tls enabled

--- pkg/v1/dragonfly/dragonfly_helm_release.go
+++ pkg/v1/dragonfly/dragonfly_helm_release.go
@@ -2,8 +2,8 @@ package dragonfly
 
 import (
 	"fmt"
-	"github.com/fjarm/infrastructure/pkg/v1/certmanager"
 	"github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes"
+	"github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/apiextensions"
 	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/core/v1"
 	helmv4 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/helm/v4"
 	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/meta/v1"
@@ -12,17 +12,34 @@ import (
 )
 
 const (
+	certificateName          = "dragonfly-server-tls"
 	chartRepo                = "oci://ghcr.io/dragonflydb/dragonfly/helm/dragonfly"
 	chartNamespace           = "dragonfly"
 	chartVersion             = "v1.30.3"
 	exportDragonflyNamespace = "dragonflyNamespace"
 	exportDragonflyStatus    = "dragonflyStatus"
 	helmChartName            = "dragonfly"
+	tlsMountPath             = "/etc/dragonfly/tls"
 )
 
 // DeployDragonfly attempts to deploy DragonflyDB using the official Helm chart.
 func DeployDragonfly(ctx *pulumi.Context, provider *kubernetes.Provider, deps []pulumi.Resource) error {
 	ns, err := newDragonflyNamespace(ctx)
+	if err != nil {
+		return err
+	}
+
+	certArgs, err := newDragonflyCertificateArgs(ns)
+	if err != nil {
+		return err
+	}
+
+	cert, err := apiextensions.NewCustomResource(
+		ctx,
+		certificateName,
+		certArgs,
+		pulumi.DependsOn(deps),
+	)
 	if err != nil {
 		return err
 	}
@@ -33,14 +50,15 @@ func DeployDragonfly(ctx *pulumi.Context, provider *kubernetes.Provider, deps []
 		helmChartName,
 		chartArgs,
 		pulumi.Provider(provider),
-		pulumi.DependsOn(deps),
+		pulumi.DependsOn(append(deps, cert)),
 	)
 	if err != nil {
 		return err
 	}
 
-	ctx.Export(exportDragonflyNamespace, ns)
-	ctx.Export(exportDragonflyStatus, dragonflyChart)
+	ctx.Export(exportDragonflyNamespace, ns.Metadata.Name())
+	ctx.Export(exportDragonflyStatus, dragonflyChart.ToChartOutput())
+	ctx.Export(certificateName, cert.OtherFields)
 	return nil
 }
 
@@ -58,10 +76,14 @@ func newDragonflyHelmChartArgs(ns *corev1.Namespace) *helmv4.ChartArgs {
 			"extraArgs": pulumi.StringArray{
 				pulumi.String("--cluster_mode=emulated"),
 				pulumi.String("--admin_port=8000"),
-				pulumi.String(fmt.Sprintf("--dbfilename=my-dump-%s}", time.Now().Format(time.RFC3339))),
-				pulumi.String("--snapshot_cron=* * * * *"), // Snapshot every minute
+				pulumi.String(fmt.Sprintf("--dbfilename=my-dump-%s", time.Now().Format(time.RFC3339))),
+				pulumi.String("--snapshot_cron=0 * * * *"), // Snapshot every hour
 				// TODO(2025-06-06): Update this to be more secure
 				pulumi.String("--requirepass=password"),
+				pulumi.String("--tls"),
+				//pulumi.String(fmt.Sprintf("--tls_ca_cert_file=%s/ca.crt", tlsMountPath)),
+				pulumi.String(fmt.Sprintf("--tls_cert_file=%s/tls.crt", tlsMountPath)),
+				pulumi.String(fmt.Sprintf("--tls_key_file=%s/tls.key", tlsMountPath)),
 			},
 			"podSecurityContext": pulumi.Map{
 				"fsGroup": pulumi.Int(2000),
@@ -76,15 +98,24 @@ func newDragonflyHelmChartArgs(ns *corev1.Namespace) *helmv4.ChartArgs {
 				"runAsNonRoot":           pulumi.Bool(true),
 				"runAsUser":              pulumi.Int(1000),
 			},
-			"tls": pulumi.Map{
-				"enabled":     pulumi.Bool(true),
-				"createCerts": pulumi.Bool(true),
-				"issuer": pulumi.Map{
-					"kind": pulumi.String("ClusterIssuer"),
-					"name": pulumi.String(certmanager.InternalClusterIssuerName),
+			"extraVolumes": pulumi.MapArray{
+				pulumi.Map{
+					"name": pulumi.String("tls"),
+					"secret": pulumi.Map{
+						"secretName": pulumi.String(certificateName),
+					},
 				},
-				"cert": pulumi.String("tls.crt"),
-				"key":  pulumi.String("tls.key"),
+			},
+			"extraVolumeMounts": pulumi.MapArray{
+				pulumi.Map{
+					"name":      pulumi.String("tls"),
+					"mountPath": pulumi.String(tlsMountPath),
+				},
+			},
+			"podAnnotations": pulumi.StringMap{
+				"config.kubernetes.io/depends-on": pulumi.String(
+					fmt.Sprintf("/namespaces/dragonfly/Secret/%s", certificateName),
+				),
 			},
 			"replicaCount": pulumi.Int(1),
 			"resources": pulumi.Map{
