81be531154ddd066a36097a091bccc668d655558
Jeremy Muhia <jmuhia80@gmail.com>
2025-06-11 10:33:00 -0400

    No new line for health probe script

--- pkg/v1/dragonfly/dragonfly_health_probe.go
+++ pkg/v1/dragonfly/dragonfly_health_probe.go
@@ -1,7 +1,6 @@
 package dragonfly
 
-const healthProbe = `
-#!/bin/sh
+const healthProbe = `#!/bin/sh
 
 echo PING | openssl s_client -connect 127.0.0.1:6379 -CAfile /etc/dragonfly/tls/ca.crt -quiet -no_ign_eof
 
