4f78bd873cdb85746e04a43d6a4699e435a86d40
Jeremy Muhia <jmuhia80@gmail.com>
2025-07-05 13:50:32 -0400

    Supply ACLs through config file that is templated

--- pkg/v1/valkey/valkey_user_acl.go
+++ pkg/v1/valkey/valkey_user_acl.go
@@ -3,10 +3,6 @@ package valkey
 import (
 	"bytes"
 	"fmt"
-	"github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes"
-	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/core/v1"
-	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/meta/v1"
-	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 	"text/template"
 )
 
@@ -14,11 +10,7 @@ import (
 // or a result containing `<nil>`.
 var ErrTemplateParsingError = fmt.Errorf("error parsing ACL file template")
 
-const (
-	aclSecretName = "valkey-acl"
-)
-
-const aclTemplate = `
+const configTemplate = `
 {{ range $index, $user := .Users }}
 user {{ $user.Username }} on >{{ $user.Password }} -@ALL {{ range $cmd := $user.EnabledCommands }}{{ $cmd }} {{ end }}
 {{ end }}
@@ -36,53 +28,11 @@ type valkeyUser struct {
 	EnabledCommands []string
 }
 
-// deployValkeyUserACLSecret composes a string containing the contents of an ACL file. That string is then deployed as
-// a secret for Valkey to use.
-func deployValkeyUserACLSecret(
-	ctx *pulumi.Context,
-	namespace *corev1.Namespace,
-	provider *kubernetes.Provider,
-	deps []pulumi.Resource,
-) (*corev1.Secret, string, error) {
-	aclContent, err := newValkeyUserACL(
-		// TODO(2025-06-30): Add support to the template for creating users with no password
-		&valkeyUser{
-			Username:        "test",
-			Password:        "test",
-			EnabledCommands: []string{"+AUTH", "+PING", "+GET", "+SET", "~*"},
-		},
-		&valkeyUser{
-			Username:        "default",
-			Password:        "password",
-			EnabledCommands: []string{"+@ALL", "~*"},
-		},
-	)
-	if err != nil {
-		return nil, "", err
-	}
-
-	aclSecretArgs := newValkeyUserACLSecretArgs(
-		namespace,
-		aclContent,
-	)
-	aclSecret, err := corev1.NewSecret(
-		ctx,
-		aclSecretName,
-		aclSecretArgs,
-		pulumi.Provider(provider),
-		pulumi.DependsOn(deps),
-	)
-	if err != nil {
-		return nil, "", err
-	}
-	return aclSecret, aclContent, nil
-}
-
 // newValkeyUserACL uses text templating to compose the contents of an ACL file as a string.
 func newValkeyUserACL(
 	users ...*valkeyUser,
 ) (string, error) {
-	tmp, err := template.New("valkey.acl").Parse(aclTemplate)
+	tmp, err := template.New("valkey.acl").Parse(configTemplate)
 	if err != nil {
 		return "", err
 	}
@@ -104,26 +54,4 @@ func newValkeyUserACL(
 	}
 
 	return contents, nil
-}
-
-// newValkeyUserACLSecretArgs creates the Secret specs that contain the Valkey ACL file contents.
-func newValkeyUserACLSecretArgs(
-	namespace *corev1.Namespace,
-	aclContent string,
-) *corev1.SecretArgs {
-	return &corev1.SecretArgs{
-		ApiVersion: pulumi.String("v1"),
-		Kind:       pulumi.String("Secret"),
-		Type:       pulumi.String("Opaque"),
-		Metadata: metav1.ObjectMetaArgs{
-			Name:      pulumi.String(aclSecretName),
-			Namespace: namespace.Metadata.Name(),
-			Labels: pulumi.StringMap{
-				"app": pulumi.String(clusterAppLabel),
-			},
-		},
-		StringData: pulumi.StringMap{
-			aclSecretName: pulumi.String(aclContent),
-		},
-	}
 }
